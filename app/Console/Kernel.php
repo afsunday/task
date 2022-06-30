012641febf933bfd6e8a50c1edc7de3eeae38142
Afuwape sunday <55183401+inlogicstudio@users.noreply.github.com>
2022-06-30 11:42:51 +0100

    update command

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -25,7 +25,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command('send:newsLetter')->everyDay();
+        $schedule->command('send:newsLetter')->everyMinute();
     }
 
     /**
